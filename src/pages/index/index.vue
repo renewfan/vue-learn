6e2a3e1b1443d09aab0e4854bbe2bba129164f86
fjy <178675994@qq.com>
2019-08-20 18:26:41 +0800

    初识

---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>this is index</h1>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Welcome to Index'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
