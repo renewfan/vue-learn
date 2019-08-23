bc9b385372856379546f59653a5356641f22c30f
fjy <178675994@qq.com>
2019-08-23 18:26:25 +0800

    首页开始

---
+++ src/pages/index/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header">
+    <div class="header-l">
+      <div class="iconfont backicon">&#xe624;</div>
+    </div>
+    <div class="header-c">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-r">
+      城市
+      <span class="iconfont city-icon">&#xe64a;</span></div>
+  </div>
+  <!-- 1rem=html font-size = 50rpx (reset.css 中进行了设置 font-size = 50rpx) -->
+</template>
+
+<script>
+export default {
+  name: 'IndexHeader',
+  data () {
+    return {
+      msg: 'Welcome to IndexHeader'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/global.styl';
+  .header
+    display:flex
+    line-height :.86rem
+    color : #fff
+    background : $bgcolor
+    .header-l
+      width : .64rem
+      float : left
+      .backicon
+        text-align : center
+        font-size : .4rem
+    .header-c
+      flex : 1
+      height : .64rem
+      line-height : .64rem
+      color : #cccccc
+      margin : .12rem 0 0 .2rem
+      padding-left : .2rem
+      background : #ffffff
+      border-radius : .1rem
+      text-align : left
+    .header-r
+      width :1.24rem
+      float : right
+      text-align : center
+      .city-icon
+        margin-left : -.04rem
+        font-size : .24rem
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,22 @@
 <template>
-  <h1>{{msg}}</h1>
+  <!-- <h1>{{msg}}</h1> -->
+  <!-- 组件使用 -->
+  <index-header></index-header>
 </template>
 
 <script>
+// 引入组件
+import IndexHeader from './components/Header'
 export default {
   name: 'Index',
   data () {
     return {
       msg: 'Welcome to Index'
     }
+  },
+  // 注册组件
+  components: {
+    IndexHeader
   }
 }
 </script>
